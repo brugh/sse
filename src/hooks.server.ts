569c3a1b2e41bafad8b7cba64e554cc88e8b5417
brugh <hercovanbrug@hotmail.com>
2023-10-28 12:20:32 +0200

    gitting

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,10 @@
 import { REDISURL, REDISTOKEN } from '$env/static/private';
+import type { Handle } from '@sveltejs/kit';
 
 import { Redis } from '@upstash/redis'
 
 const redis = new Redis({ url: REDISURL, token: REDISTOKEN });
-export const handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
   event.locals.redis = redis;
   return resolve(event);
 };
